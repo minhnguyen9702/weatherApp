96427880529ddc69b59a206900f83c7f6b1ae837
minhnguyen9702 <minhnguyen9702@gmail.com>
2024-10-16 10:56:53 +0700

    add call getWeather on button click

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
-async function getWeather() {
-  const response = await fetch(
-    "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/London,UK?key=B4JF83Z5AJ2FAK5FQMMPFMSJE",
-    { mode: "cors" }
-  );
-  const data = await response.json();
-  console.log(data)
+async function getWeather(searchTerm) {
+  try {
+    const response = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${searchTerm}?key=B4JF83Z5AJ2FAK5FQMMPFMSJE`,
+      { mode: "cors" }
+    );
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error("Failed to fetch weather data:", error);
+  }
 }
-getWeather()
+
+const searchInput = document.getElementById("searchInput");
+const searchBtn = document.getElementById("searchBtn");
+
+searchBtn.addEventListener("click", () => {
+  const term = searchInput.value;
+  if (term) {
+    getWeather(searchInput.value);
+  } else {
+    console.error("Please Enter a Valid Location");
+  }
+});
