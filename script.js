f29b8fdebf4670f427a31224686d98e719a0fa7c
minhnguyen9702 <minhnguyen9702@gmail.com>
2024-10-16 11:46:04 +0700

    make weather conditions display in dom

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ async function getWeather(searchTerm) {
       throw new Error(`HTTP error! Status: ${response.status}`);
     }
 
-    const data = await response.sjson();
+    const data = await response.json();
     return processWeatherData(data);
   } catch (error) {
     console.error("Failed to fetch weather data:", error);
@@ -21,20 +21,24 @@ function processWeatherData(weatherData) {
   let temperature = weatherData.currentConditions.temp;
   let conditions = weatherData.currentConditions.conditions;
 
-  return {resolvedAddress, temperature, conditions};
+  return { resolvedAddress, temperature, conditions };
 }
 
 const searchInput = document.getElementById("searchInput");
 const searchBtn = document.getElementById("searchBtn");
+
+const addressDiv = document.getElementById("resolvedAddress")
+const temperatureDiv = document.getElementById("temperatureDiv")
+const conditionsDiv = document.getElementById("conditionsDiv")
 
 searchBtn.addEventListener("click", async () => {
   const term = searchInput.value;
   if (term) {
     const weatherData = await getWeather(term);
     if (weatherData) {
-        console.log(weatherData.resolvedAddress);
-        console.log(weatherData.temperature);
-        console.log(weatherData.conditions);
+      addressDiv.innerText = weatherData.resolvedAddress;
+      temperatureDiv.innerText = weatherData.temperature;
+      conditionsDiv.innerText = weatherData.conditions;
     }
   } else {
     console.error("Please Enter a Valid Location");
